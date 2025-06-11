29f71aa19da618ea1dbf3a9de56cb40e505132eb
superscalar <65932360+superscalar@users.noreply.github.com>
2025-06-10 23:22:48 -0300

    Control postprocessing effects via keyboard

--- Assets/Content/Scenes/TestSceneScript.cs
+++ Assets/Content/Scenes/TestSceneScript.cs
@@ -1,39 +1,42 @@
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 
 public class TestSceneScript : MonoBehaviour
 {
     PostProcessVolume volume;
-    ChromaticAberration ca;
-
+    
     void Start()
     {
         GameObject volumeObj = GameObject.Find("Postprocessing Volume");
-        if (volumeObj != null)
-        {
+        if (volumeObj != null) {
             volume = volumeObj.GetComponent<PostProcessVolume>();
-            if (volume != null)
-            {
+            if (volume != null) {
                 Debug.LogWarning("Found volume " + volume);
-                /* ca = new ChromaticAberration();
-                FloatParameter fp = new FloatParameter(); fp.value = 1.0f; // but why?
-                ca.intensity = fp; */
             }
         }
-        
+        ca_enabled = false;
     }
 
+    bool ca_enabled;
     void Update() {
-        if (Input.GetKeyDown(KeyCode.C)) // C for chromatic aberration
-        {
-            volume.profile.AddSettings<ChromaticAberration>();
-
-            FloatParameter fp = new FloatParameter(); fp.value = 1.0f;
-            BoolParameter bp = new BoolParameter(); bp.value = true;
-            volume.profile.GetSetting<ChromaticAberration>().enabled = bp;
-            volume.profile.GetSetting<ChromaticAberration>().intensity = fp;
-
-            
+        if (Input.GetKeyDown(KeyCode.C)) { // C for chromatic aberration
+            ChromaticAberration ca;
+            volume.profile.TryGetSettings(out ca);
+            ca.intensity.value = ca_enabled ? 1.0f : 0.0f;
+            ca_enabled = !ca_enabled;
         }
+
+        /* Later, try to figure how to do to this
+        if (Input.GetKeyDown(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.C)) {
+            ChromaticAberration ca;
+            volume.profile.TryGetSettings(out ca);
+            ca.intensity.value = 0.0f;
+        } else if (Input.GetKeyDown(KeyCode.C)) {
+            ChromaticAberration ca;
+            volume.profile.TryGetSettings(out ca);
+            ca.intensity.value = 1.0f;
+        }
+        */
     }
 }
